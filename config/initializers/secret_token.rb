774c0cea13840b41b331835a20b61d881032a3f8
Rafał Mleczko <r.mleczko@gamil.com>
2013-11-05 14:37:26 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyServiceMod::Application.config.secret_token = '27372e0fb50198a7f05d50935cccda21218cce851f9a891251f7de459d16677b3438a14ec30d17e5652efecf4597b6d942a41bfaf9069c7ae87e8ae5513e4a0e'
