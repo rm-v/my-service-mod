c3069ce4c531c49b414bf01af784f0a80e00156e
Rafał Mleczko <r.mleczko@gmail.com>
2013-11-13 12:15:22 +0100

    Added routs for Branches controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 MyServiceMod::Application.routes.draw do
   get "static_pages/home"
   root :to => 'static_pages#home'
+
+  resources :branches
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
