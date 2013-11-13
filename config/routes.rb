2eee637bf15f0e003983f81f07c4b41a3f551e6c
Rafał Mleczko <r.mleczko@gmail.com>
2013-11-13 14:46:17 +0100

    Added Services controller and views for REST actions.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ MyServiceMod::Application.routes.draw do
 
   resources :branches
   resources :trades
+  resources :services
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
