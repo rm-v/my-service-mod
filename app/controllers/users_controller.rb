00505a7d772066992f456f795204f63b198b31be
Rafał Mleczko <r.mleczko@gmail.com>
2013-11-21 15:02:31 +0100

    Delete and Update actions for 'User" model were repaired.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,15 +29,22 @@ class UsersController < ApplicationController
   
   
   def edit
-    
+    @user = User.find params[:id]
   end
 
   def update
-    
+    @user = User.find params[:id]
+    if @user.update_attributes params[:user]
+      redirect_to user_path(@user.id), notice: "Dane zmieniono."
+    else
+      render action: "edit"
+    end
   end
   
   def destroy
-    
+    @user = User.find params[:id]
+    @user.destroy
+    redirect_to users_path
   end
   
 end
